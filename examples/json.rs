1027f52be54d5575aa39338ae5fed924a3168fde
Eric B. Ridge <eebbrr@gmail.com>
2020-05-30 14:21:42 -0600

    first run at proper error handling.

--- examples/json.rs
+++ examples/json.rs
@@ -2,7 +2,14 @@ use postgres_parser::*;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let parse_list = parse_query(args.get(1).expect("no arguments")).unwrap();
+    let query_string = args.get(1).expect("no arguments");
+    let parse_list = match parse_query(query_string) {
+        Ok(query) => query,
+        Err(e) => {
+            eprintln!("{:?}", e);
+            return;
+        }
+    };
     let as_json = serde_json::to_string_pretty(&parse_list).expect("failed to convert to json");
 
     println!("{}", as_json);
--- includes.h
+++ includes.h
@@ -1,27 +1,8 @@
-//#include <stdint.h>
-//#include <stdbool.h>
-//
-//#define PGDLLIMPORT
-//#define FLEXIBLE_ARRAY_MEMBER
-//#define USE_STDBOOL 1
-//#define NULL 0
-//#define PG_INT32_MAX 2147483647
-//
-//#define bits32 uint32_t
-//#define uint32 uint32_t
-//#define uint64 uint64_t
-//#define int32 int32_t
-//#define int16 int16_t
-//
-//typedef unsigned int Index;
-//typedef unsigned int Oid;
-//typedef uintptr_t Datum;
-//typedef size_t Size;
-
 #include "postgres.h"
 #include "nodes/memnodes.h"
 #include "nodes/nodes.h"
 #include "nodes/parsenodes.h"
 #include "parser/parser.h"
+#include "utils/elog.h"
 #include "utils/memutils.h"
 #include "utils/palloc.h"
